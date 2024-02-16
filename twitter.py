053e438c3a48a3232e888b856282ce3021a968a8
TAKOS <109962559+FncTAKOS@users.noreply.github.com>
2024-02-16 22:31:08 +0100

    Add files via upload

---
+++ twitter.py
@@ -0,0 +1,19 @@
+import tweepy
+
+def main():
+
+    api_key = ""
+    api_secret = ""
+    bearer_token = r""
+    access_token = ""
+    access_token_secret = ""
+
+    client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
+
+    try:
+        client.create_tweet(text="Hello World!")
+        print("Tweet posted successfully")
+    except Exception as error:
+        print(f"Tweet posting failed => [{error}]")
+
+main()
